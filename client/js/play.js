da0dad6780281aa42cb83407239b4685c03c5bb1
Vija02 <vija02@newhuaweiapstudent-10-58-211-80.ncl.ac.uk>
2019-03-23 17:48:57 +0000

    player movement

--- client/js/play.js
+++ client/js/play.js
@@ -1,139 +1,141 @@
-var map;
-var layer;
-var bullet_array = [];
-var socket;
-var other_player = {};
-
-players = [1,2,3,4]
+var map
+var layer
+var bullet_array = []
+var socket
+var other_player = {}
 
 const player_map = players.map(player => {
-  return {
-    id: player,
-    sprite: null,//Will hold the sprite when it's created 
-    speed:2, // This is the parameter for how fast it should move 
-    shot:false,
-    update: function(){
-        if(player === 1){
-          this.sprite.animations.add('down', [0,1,2,3], 6);
-          this.sprite.animations.add('up', [4,5,6,7], 6);
-          this.sprite.animations.add('side', [8,9,10], 6);
-        }
-        if(player === 2){
-          this.sprite.animations.add('down', [11,12,13,14], 6);
-          this.sprite.animations.add('up', [15,16,17,18], 6);
-          this.sprite.animations.add('side', [19,20,21], 6);
-        }
-        if(player === 3){
-          this.sprite.animations.add('down', [22,23,24,25], 6);
-          this.sprite.animations.add('up', [26,27,28,29], 6);
-          this.sprite.animations.add('side', [30,31,32], 6);
-        }
-        if(player === 4){
-          this.sprite.animations.add('down', [33,34,35,36], 6);
-          this.sprite.animations.add('up', [37,38,39,40,], 6);
-          this.sprite.animations.add('side', [41,42,43], 6);
-        }
-        
+	return {
+		id: player,
+		sprite: null, //Will hold the sprite when it's created
+		speed: 2, // This is the parameter for how fast it should move
+		shot: false,
+		update: function() {
+			if (player === 1) {
+				this.sprite.animations.add('down', [0, 1, 2, 3], 6)
+				this.sprite.animations.add('up', [4, 5, 6, 7], 6)
+				this.sprite.animations.add('side', [8, 9, 10], 6)
+			}
+			if (player === 2) {
+				this.sprite.animations.add('down', [11, 12, 13, 14], 6)
+				this.sprite.animations.add('up', [15, 16, 17, 18], 6)
+				this.sprite.animations.add('side', [19, 20, 21], 6)
+			}
+			if (player === 3) {
+				this.sprite.animations.add('down', [22, 23, 24, 25], 6)
+				this.sprite.animations.add('up', [26, 27, 28, 29], 6)
+				this.sprite.animations.add('side', [30, 31, 32], 6)
+			}
+			if (player === 4) {
+				this.sprite.animations.add('down', [33, 34, 35, 36], 6)
+				this.sprite.animations.add('up', [37, 38, 39, 40], 6)
+				this.sprite.animations.add('side', [41, 42, 43], 6)
+			}
 
-        this.sprite.frame = 0;
-        this.sprite.sfx = {};
-        this.sprite.sfx.collide = game.add.audio('hit');
+			this.sprite.frame = 0
+			this.sprite.sfx = {}
+			this.sprite.sfx.collide = game.add.audio('hit')
 
-        if(player === 1){
-          if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
-            this.sprite.body.y += -this.speed; 
-            this.sprite.animations.play('up');
-          }
-          if(game.input.keyboard.isDown(Phaser.Keyboard.S)){
-              this.sprite.body.y += this.speed;
-              this.sprite.animations.play('down');
-          }
-          if(game.input.keyboard.isDown(Phaser.Keyboard.A)){
-              this.sprite.body.x += -this.speed;
-              this.sprite.animations.play('side');
-          }
-          if(game.input.keyboard.isDown(Phaser.Keyboard.D)){
-              this.sprite.body.x += this.speed;
-              this.sprite.animations.play('side');
-          }
-        }
-        if(player === 2){
-            if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-              this.sprite.body.y += -this.speed; 
-              this.sprite.animations.play('up');
-          }
-          if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-              this.sprite.body.y += this.speed;
-              this.sprite.animations.play('down');
-          }
-          if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-              this.sprite.body.x += -this.speed;
-              this.sprite.animations.play('side');
-          }
-          if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-              this.sprite.body.x += this.speed;
-              this.sprite.animations.play('side');
-          }
-        }
-      
-      
-        // Tell the server we've moved 
-        // socket.emit('move-player',{x:this.sprite.x,y:this.sprite.y});
-    },
-  
-    onCollide: function(){
-      this.sprite.sfx.collide.play();
-        game.add.tween(this.sprite).to({alpha:1}, 1000, Phaser.Easing.Linear.None, true);
-        setTimeout(
-        () => {
-            game.add.tween(this.sprite).to({alpha:0}, 1000, Phaser.Easing.Linear.None, true);
-        },
-        1000
-        ); 
-    },
-  
-    collideSound: function(){
-      this.sprite.sfx.collide.play();
-    }
-  }
-});
+			const theDirection = playersMove.find(e => e.playerId === this.id).direction
 
-var player = {
-}
+			if (theDirection === 'N') {
+				this.sprite.body.y += -this.speed
+				this.sprite.animations.play('up')
+			}
+			if (theDirection === 'S') {
+				this.sprite.body.y += this.speed
+				this.sprite.animations.play('down')
+			}
+			if (theDirection === 'W') {
+				this.sprite.body.x += -this.speed
+				this.sprite.animations.play('side')
+			}
+			if (theDirection === 'E') {
+				this.sprite.body.x += this.speed
+				this.sprite.animations.play('side')
+			}
+
+			// Tell the server we've moved
+			// socket.emit('move-player',{x:this.sprite.x,y:this.sprite.y});
+		},
+
+		onCollide: function() {
+			this.sprite.sfx.collide.play()
+			game.add.tween(this.sprite).to({ alpha: 1 }, 1000, Phaser.Easing.Linear.None, true)
+			setTimeout(() => {
+				game.add.tween(this.sprite).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true)
+			}, 1000)
+		},
+
+		collideSound: function() {
+			this.sprite.sfx.collide.play()
+		},
+	}
+})
+
+var player = {}
 
 var playState = {
-  create: function() {
-    //Load tileset to the game
-    map = game.add.tilemap('map');
-    map.addTilesetImage('dungeon', 'tiles');
-   
-    map.setCollision([1,2,3,4,5,6,11,16,21,26,31,36,41,41,42,43,44,45,46,51,52,53,54,55,56,67,68,79]);
-    layer = map.createLayer('Tile Layer 1');
-    layer.resizeWorld();
+	create: function() {
+		//Load tileset to the game
+		map = game.add.tilemap('map')
+		map.addTilesetImage('dungeon', 'tiles')
 
-    player_map.forEach((player) => {
-      player.sprite = game.add.sprite(50, 40, 'characters');
-      game.add.existing(player.sprite);
-      game.physics.enable(player.sprite, Phaser.Physics.ARCADE);
-      player.sprite.body.collideWorldBounds = true;
-      //game.add.tween(player.sprite).to({alpha:0}, 200, Phaser.Easing.Linear.None, true);
-    });
+		map.setCollision([
+			1,
+			2,
+			3,
+			4,
+			5,
+			6,
+			11,
+			16,
+			21,
+			26,
+			31,
+			36,
+			41,
+			41,
+			42,
+			43,
+			44,
+			45,
+			46,
+			51,
+			52,
+			53,
+			54,
+			55,
+			56,
+			67,
+			68,
+			79,
+		])
+		layer = map.createLayer('Tile Layer 1')
+		layer.resizeWorld()
 
+		player_map.forEach(player => {
+			player.sprite = game.add.sprite(50, 40, 'characters')
+			game.add.existing(player.sprite)
+			game.physics.enable(player.sprite, Phaser.Physics.ARCADE)
+			player.sprite.body.collideWorldBounds = true
+			//game.add.tween(player.sprite).to({alpha:0}, 200, Phaser.Easing.Linear.None, true);
+		})
 
-    // socket = io("http://vija02.localhost.run"); // This triggers the 'connection' event on the server
-  },
-  render: function(){
-    game.debug.geom(this.bar,'#0fffff')
-  },
-  update: function() {
-    player_map.forEach((player) => {
-      player.update();
-      game.physics.arcade.collide(player.sprite, layer, function() {
-        player.onCollide();
-      });
-      game.physics.arcade.collide(player.sprite, player.sprite, function() {
-        player.collideSound();
-      })
-    })
-  }
-};
+		// socket = io("http://vija02.localhost.run"); // This triggers the 'connection' event on the server
+	},
+	render: function() {
+		game.debug.geom(this.bar, '#0fffff')
+	},
+	update: function() {
+		player_map.forEach(player => {
+			player.update()
+			game.physics.arcade.collide(player.sprite, layer, function() {
+				player.onCollide()
+			})
+			game.physics.arcade.collide(player.sprite, player.sprite, function() {
+				player.collideSound()
+			})
+		})
+	},
+}
