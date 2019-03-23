a0fdd7d7c828e33b3ce5953cbc0baa44416eb4a4
Vija02 <vija02@newhuaweiapstudent-10-58-211-80.ncl.ac.uk>
2019-03-23 17:06:18 +0000

    feat: connect to server

--- client/js/title.js
+++ client/js/title.js
@@ -1,11 +1,49 @@
+var socket;
+
+// If claimed
+let connected = false;
+
+let players = []
+
+
+let playerLabel;
+
 var titleState = {
 	create: function (){
-		var nameLabel = game.add.text(160, 80, "Click anywhere to start", {
+		game.add.text(100, 200, "Click anywhere to start", {
 			font: '14px Space Mono', fill: '#ffffff'
 		});
+
+		playerLabel = game.add.text(100, 80, "Player count: 0", {
+			font: '14px Space Mono', fill: '#ffffff'
+		});
+
+		socket = io('http://vija02.localhost.run');
+
+		// Connect stuff
+		socket.on('connect', () => {
+			socket.emit('claimWeb')
+		})
+		socket.on('claimWeb', function () {
+			connected = true
+			socket.on('event', function (data) { });
+			socket.on('disconnect', function () { });
+		});
+
+		// Listen for users
+		socket.on("syncUser", (data) => {
+			players = data.users
+		})
+		
+		socket.on('disconnect', function () {
+			connected = false
+		});
+
 	},
 	update: function(){
-		if (game.input.activePointer.isDown) {
+		playerLabel.setText(`Player count: ${players.length}`);
+
+		if (game.input.activePointer.isDown && connected && players.length > 1) {
 			game.state.start('play')
 		}
 	}
